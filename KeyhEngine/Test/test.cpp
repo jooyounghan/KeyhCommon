e8b1d0fe7f2da34776e575877434c4b0047d35a5
YounghanJoo <rsfnqf98@naver.com>
2026-06-08 01:03:55 +0900

    Add HashContainerCommon for Common Logic of HashMap and HashSet

--- KeyhEngine/Test/test.cpp
+++ KeyhEngine/Test/test.cpp
@@ -1,3 +1,5 @@
+#define NOMINMAX
+
 #include <crtdbg.h>
 #include <iostream>
 #include <chrono>
@@ -13,30 +15,38 @@ using namespace keyh;
 
 int main()
 {
-	AssertUtil::gAssertHandler = std::move(AssertDelegate().bind([](const char* message) -> bool {
-        int result = ::MessageBoxA(
-            nullptr,
-            message,
-            "Assertion Failed!",
-            MB_YESNO | MB_ICONERROR | MB_TOPMOST | MB_SETFOREGROUND
-        );
+	//AssertUtil::gAssertHandler = std::move(AssertDelegate().bind([](const char* message) -> bool {
+ //       int result = ::MessageBoxA(
+ //           nullptr,
+ //           message,
+ //           "Assertion Failed!",
+ //           MB_YESNO | MB_ICONERROR | MB_TOPMOST | MB_SETFOREGROUND
+ //       );
 
-        if (result == IDYES)
-        {
-            return true;
-        }
+ //       if (result == IDYES)
+ //       {
+ //           return true;
+ //       }
 
-        return false;
-        })
-    );
+ //       return false;
+ //       })
+ //   );
 
-	KEYH_ASSERT_ARGS(1 + 1 != 2, "Math is broken: %d + %d != %d", 1, 1, 2);
-    
-	printf("This line will not be reached if the assertion fails and the user chooses to break into the debugger.\n");
+	//KEYH_ASSERT_ARGS(1 + 1 != 2, "Math is broken: %d + %d != %d", 1, 1, 2);
+ //   
+	//printf("This line will not be reached if the assertion fails and the user chooses to break into the debugger.\n");
 
 	HashMap<int, std::string> map;
 	map.insert(1, "one");
     map.find(1);
 	map.remove(1);
+
+	for (size_t i = 0; i < HashContainer::kHashContainerCapacityTableSize; ++i)
+	{
+		std::cout << "Capacity for size " << i << ": " << HashContainer::getHashContainerCapacityTable()[i] << " expected: " << MathUtil::nextPrime(HashContainer::kInitialCapacity * (1ULL << i)) << std::endl;
+
+	}
+
+    StaticArray<int32, HashContainer::kHashContainerCapacityTableSize> capacityTable = HashContainer::getHashContainerCapacityTable();
     return 0;
 }
