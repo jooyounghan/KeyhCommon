e08189685e453b517c17e5ed06848ba7bf289cab
YounghanJoo <rsfnqf98@naver.com>
2026-06-18 00:10:51 +0900

    modify pch

--- KeyhEngine/CommonBase/AssertUtil.h
+++ KeyhEngine/CommonBase/AssertUtil.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <cstdlib>
+
 #include "Delegate.h"
 
 #if defined(__clang__)
--- KeyhEngine/CommonBase/CommonBasePch.h
+++ KeyhEngine/CommonBase/CommonBasePch.h
@@ -4,9 +4,3 @@
 #include <malloc.h>
 #include <cstring>
 #pragma endregion
-
-#pragma region Assert
-#include <cstdlib>
-#include <cstdio>
-#include <intrin.h>
-#pragma endregion
