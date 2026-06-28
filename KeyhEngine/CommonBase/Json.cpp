20464f1d6ac0bcf19ab12a9fae73576b5c6be8be
YounghanJoo <rsfnqf98@naver.com>
2026-06-29 00:32:16 +0900

    add TapeElement

--- KeyhEngine/CommonBase/Json.cpp
+++ KeyhEngine/CommonBase/Json.cpp
@@ -1,2 +1,18 @@
 #include "CommonBasePch.h"
 #include "Json.h"
+
+namespace keyh
+{
+    void JsonDocument::TapeElement::setElement(TapeType type, uint64_t payload)
+    {
+        uint64_t typeVal = static_cast<uint64_t>(type);
+        _value = (typeVal << 56) | (payload & 0x00FFFFFFFFFFFFFFULL);
+    }
+    
+    void JsonDocument::TapeElement::setStringElement(uint64_t offset, uint32_t length)
+    {
+        uint64_t typeVal = static_cast<uint64_t>(TapeType::String);
+        uint64_t lengthVal = static_cast<uint64_t>(length);
+        _value = (typeVal << 56) | ((lengthVal & 0xFFFFFF) << 32) | (offset & 0xFFFFFFFF);
+    }
+}
--- KeyhEngine/CommonBase/Json.h
+++ KeyhEngine/CommonBase/Json.h
@@ -1,17 +1,39 @@
 #pragma once
 #include "CommonCore.h"
 #include "Vector.h"
-#include "StringView.h"
-#include "DynamicBuffer.h"
+
 namespace keyh
 {
-	using TapeElement = uint64;
-
 	class JsonDocument
 	{
+		enum class TapeType : uint8
+		{
+			ObjectStart = '{',
+			ObjectEnd = '}',
+			ArrayStart = '[',
+			ArrayEnd = ']',
+			String = '"',
+			Number = '0',
+		};
+
+		class TapeElement
+		{
+		private:
+			uint64 _value;
+
+		public:
+            void setElement(TapeType type, uint64_t payload);
+            void setStringElement(uint64_t offset, uint32_t length);
+
+		public:
+            inline TapeType getType() const { return static_cast<TapeType>(_value >> 56); }
+            inline uint64_t getPayload() const { return _value & 0x00FFFFFFFFFFFFFFULL; }
+            inline uint64_t getStringOffset() const { return _value & 0xFFFFFFFF; }
+            inline uint32_t getStringLength() const { return static_cast<uint32_t>((_value >> 32) & 0xFFFFFF); }
+		};
+
 	private:
 		Vector<TapeElement> _tapeElements;
-		DynamicBuffer<char> _jsonString;
 
 	public:
 		void buildFromJsonString(const char* jsonString);
