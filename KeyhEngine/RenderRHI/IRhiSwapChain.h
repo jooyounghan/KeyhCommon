dd208058aa727d70b0d70b651650f5054eae6d0a
YounghanJoo <rsfnqf98@naver.com>
2026-07-25 14:27:03 +0900

    fix props file

--- KeyhEngine/RenderRHI/IRhiSwapChain.h
+++ KeyhEngine/RenderRHI/IRhiSwapChain.h
@@ -1,4 +1,24 @@
 ﻿#pragma once
-class IRhiSwapChain
-{};
+namespace keyh
+{
+	class IRhiTexture;
+
+	class IRhiSwapChain
+	{
+	public:
+		IRhiSwapChain() = default;
+		virtual ~IRhiSwapChain() = default;
+
+	public:
+		virtual void			present() = 0;
+		virtual void			resize(uint32 width, uint32 height) = 0;
+		virtual IRhiTexture*	getBackBuffer() = 0;
+		virtual uint32			getBackBufferCount() = 0;
+	};
+
+	class D3D12SwapChain : public IRhiSwapChain
+	{
+
+	};
+}
 
