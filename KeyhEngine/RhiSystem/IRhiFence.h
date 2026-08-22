45e6560ece4daa76af1710b9742f634fc4fdb8e4
YounghanJoo <rsfnqf98@naver.com>
2026-08-22 11:44:42 +0900

    prepare for Rhi Common Object

---
+++ KeyhEngine/RhiSystem/IRhiFence.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+namespace keyh
+{
+	class IRhiFence
+	{
+	public:
+		IRhiFence() = default;
+		virtual ~IRhiFence() = default;
+	public:
+		virtual void waitForSignal(uint64 timeoutInNanoseconds) = 0;
+	};
+}
--- KeyhEngine/RhiSystem/IRhiSampler.cpp
+++ KeyhEngine/RhiSystem/IRhiSampler.cpp
@@ -1,5 +1,9 @@
 ﻿#include "RhiSystemPch.h"
 #include "IRhiSampler.h"
+#include "D3D12Sampler.h"
+#include "D3D12RhiConvert.h"
+#include "D3D12Device.h"
+#include "D3D12GlobalResourceHeap.h"
 
 namespace keyh
 {
@@ -7,6 +11,18 @@ namespace keyh
 	{
 		FNV1aHash<RhiStaticSamplerDesc> hasher;
 		_hashValue = hasher(desc);
+	}
+
+	D3D12Sampler::D3D12Sampler(D3D12Device* device, const RhiStaticSamplerDesc& desc)
+		: IRhiSampler(desc)
+	{
+		device->getSamplerHeap()->allocate(_cpuHandle, _gpuHandle, _descriptorHeapAllocIndex);
+
+		D3D12_SAMPLER_DESC d3d12SamplerDesc = toD3D12SamplerDesc(desc);
+		device->getNativeDevice()->CreateSampler(
+			&d3d12SamplerDesc,
+			_cpuHandle
+		);
 	}
 }
 
